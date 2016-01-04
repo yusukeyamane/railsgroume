6298a0b7bdd0087452ccf5487f4f7d32ab311086
kazuku higashiguchi <higashiguchi_kazuki@yahoo.co.jp>
2016-01-04 17:23:01 +0900

    画面上はhashtag実装。でも、データが送信されない。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+
   end
 
   # GET /posts/new
@@ -27,7 +28,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.build_hashtag
     respond_to do |format|
-      if @post.save
+      if @post.save & @post.hashtag.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
         logger.debug(@post)
@@ -66,16 +67,22 @@ class PostsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
+      @restaurant = @post.restaurant
     end
 
     def form_components
-      @restaurants = Restaurant.all.pluck(:name)
+      # to make restaurant information hash
+      restaKey = Restaurant.all.pluck(:name)
+      restaValue = Restaurant.all.pluck(:id)
+      resta_ary = [restaKey,restaValue].transpose
+      @restaurants = Hash[*resta_ary.flatten]
       @price_zone = {"¥0 ~ ¥999" => 500, "¥1000 ~ ¥1999" => 1500, "¥2000 ~ ¥2999" => 2500, "¥3000 ~ ¥3999" => 3500, "¥4000 ~ ¥4999" => 4500}
       @evaluation = [1,2,3,4,5]
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:titile, :user_id, :restaurant_id, :content, :time_zone, :amount, :total_eval, :quality_eval, :service_eval, :atomos_eval, :drink_eval, :partner_eval, :visit_date, :url, hashtag_attributes: [:couple])
+      @hash_att = [:couple, :firstdate, :onesidelove, :confession, :lunch, :cafe, :dinner, :aniversary, :oneyear_ani, :onemonth_ani, :xmas, :valentine, :whiteday]
+      params.require(:post).permit(:titile, :user_id, :restaurant_id, :content, :time_zone, :amount, :total_eval, :quality_eval, :service_eval, :atomos_eval, :drink_eval, :partner_eval, :visit_date, :url, hashtag_attributes: @hash_att)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :restaurant, :autosave => true
-  has_one :hashtag #:autosave => true
-  accepts_nested_attributes_for :hashtag
+  has_one :hashtag ,:autosave => true
+  accepts_nested_attributes_for :hashtag, :restaurant
 end
